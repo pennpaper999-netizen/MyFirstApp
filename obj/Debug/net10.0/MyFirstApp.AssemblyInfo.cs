059be77fa83741c601d1b93c2699c0d36508b5f6
pennpaper999-netizen <pennpaper999@gmail.com>
2026-06-22 09:54:06 +0530

    Initial commit

--- obj/Debug/net10.0/MyFirstApp.AssemblyInfo.cs
+++ obj/Debug/net10.0/MyFirstApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyFirstApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99eb2cf4b906c338009d4de9b25af323ad32565d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyFirstApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyFirstApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
